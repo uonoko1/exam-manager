3827ac3f08a059abd9c156fd9e48b4e3ac968fba
uonoko1 <sakai.personal@gmail.com>
2024-07-10 03:11:26 +0900

    [feat] Add evaluation logic to use case layer and refactor application logic into interfaces
    - Added evaluation logic to the use case layer
    - Refactored application logic into interfaces for cleaner and more
      modular use case classes
    - Improved code maintainability and readability by decoupling
      implementation details

---
+++ app/usecases/examResult/repository/ExamResultRepository.scala
@@ -0,0 +1,13 @@
+package usecases.examResult.repository
+
+import domain.examResult.entity.ExamResult
+import domain.examResult.valueObject._
+import domain.exam.valueObject._
+import scala.concurrent.Future
+
+trait ExamResultRepository {
+  def save(examResult: ExamResult): Future[Either[String, ExamResult]]
+  def findById(examId: ExamId): Future[Either[String, Option[ExamResult]]]
+  def findByExamId(examId: ExamId): Future[Either[String, Seq[ExamResult]]]
+  def update(examResult: ExamResult): Future[Either[String, ExamResult]]
+}
